a836af93d4746abba49c70bca26260d1ffa389d3
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-08-03 23:47:37 +0200

    Delete Config.java

--- app/src/main/java/az/aldoziflaj/popmovies/Config.java
+++
@@ -1,9 +0,0 @@
-package az.aldoziflaj.popmovies;
-
-public final class Config {
-
-    public static final String API_KEY = "6e7da6d84d9ec6215cffd1c3e8924c87";
-    public static final String API_BASE_URL = "http://api.themoviedb.org/3/discover/movie";
-    public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-
-}
